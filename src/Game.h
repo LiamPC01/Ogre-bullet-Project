6fc2278192c402b88b927bc2e8dba8b094522e60
Glenn L. Jenkins <gljenkins@cardiffmet.ac.uk>
2019-03-10 21:33:35 +0000

    Finished the documentation, going to check nothing is broken

--- src/Game.h
+++ src/Game.h
@@ -62,25 +62,81 @@ private:
     Player *player;
 
 public:
+  /**
+  * Creates the object, sets all pointers to nullptr.
+  */
 	Game();
+
+  /**
+  * Distructor (virtual), as this is virtual that of the sub class will also be called.
+  */
 	virtual ~Game();
 
-	void setup();
+  /**
+  * Carries out all setup, includes lighting, scene objects.
+  */
+  void setup();
 
+  /**
+  * Sets up the camera
+  */
 	void setupCamera();
 
+  /**
+  * Quick and dirty box mesh, essentally this is a mix of the Ogre code to setup a box - from example.
+  * Added to this is the setup for the bullet3 collision box and rigid body.
+  */
 	void setupBoxMesh();
+
+  /**
+  * A copy of a quick and dirty box mesh, essentally this is a mix of the Ogre code to setup a box - from example.
+  * Added to this is the setup for the bullet3 collision box and rigid body.
+  */
   void setupBoxMesh2();
+
+  /**
+  * Player setup, this tests the Player object.
+  */
   void setupPlayer();
+
+  /**
+  * Turns on on the coffee machine.
+  */
   void setupFloor();
 
+  /**
+  * Creates, lights and adds them to the scene.  All based on the sample code, needs moving out into a level class.
+  */
 	void setupLights();
 
+  /**
+  * Overload of the keyPressed method.
+  * @param evt, a KeyboardEvent
+  */
 	bool keyPressed(const KeyboardEvent& evt);
+
+  /**
+  * Overload of the mouseMoved method.
+  * @param evt, a KeyboardEvent
+  */
 	bool mouseMoved(const MouseMotionEvent& evt);
 
+  /**
+  * Ogre wraps the game loop, but we've registered as being interested in FrameEvents (through inheritance).
+  * This method is called by the framework before rendering the frame.
+  * @param evt, FrameEvent.
+  */
 	bool frameStarted (const FrameEvent &evt);
+
+  /**
+  * Ogre wraps the game loop, but we've registered as being interested in FrameEvents (through inheritance).
+  * This method is called by the framework after rendering the frame.
+  * @param evt, FrameEvent.
+  */
 	bool frameEnded(const FrameEvent &evt);
 
+  /**
+  * Sets up the bullet environment
+  */
   void bulletInit();
 };
